1617ad131e94fd3a273d3cb2a007d4121e3d50c4
AMargaillan <adeline.margaillan@gmail.com>
2019-06-12 01:55:29 +0000

    imageblur1

---
+++ image.rb
@@ -0,0 +1,20 @@
+image = Array[
+  ([0, 0, 0, 0]),
+  ([0, 1, 0, 0]),
+  ([0, 0, 0, 1]),
+  ([0, 0, 0, 0])
+  ]
+
+
+image.each do |row|
+  row.each do |col|
+    print col.to_s + " "
+  end
+  puts
+end
+
+ 
+
+
+
+
